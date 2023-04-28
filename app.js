7ee3016b9097bd08783b130f7081f9ce58006ede
Amanda <amandabaratadev@gmail.com>
2023-04-28 17:06:02 +0100

    setting up the db

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ const express = require("express");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.APP_PORT ?? 5000;
+const port = process.env.PORT || 5001;
 
 const test = (req, res) => {
     res.send("Hello World");
   };
+
+app.use(express.json())
+
+app.use(
+  cors({
+      "origin": "http://localhost:3000"
+  })
+)
 
 app.get("/", test);
 
