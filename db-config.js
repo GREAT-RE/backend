6e68eedb8da47913b54d006fa56410138f720c0a
LuisC92 <luis.clara@wildcodeschool.com>
2023-05-22 15:24:18 +0100

    fix deploy errors

--- db-config.js
+++ db-config.js
@@ -2,13 +2,15 @@ require("dotenv").config()
 
 const mysql = require("mysql2/promise")
 
-const connection = mysql.createPool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-})
+// const connection = mysql.createPool({
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_DATABASE,
+// })
+
+const connection = mysql.createPool(process.env.DB_URL)
 
 connection
 .getConnection()
